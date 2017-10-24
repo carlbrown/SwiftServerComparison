96273fec7b3b55735bd42cc585b0126773e16809
Carl Brown <carl.brown1@ibm.com>
2017-10-24 12:55:04 -0500

    exclude valgrind/heaptrack from RAM recordings

--- bin/utility/write_memory_log.sh
+++ bin/utility/write_memory_log.sh
@@ -33,12 +33,12 @@ if [ ! -z "`which smem`" ] ; then
 fi
 top $TOP_ARG | grep 'PID' | head -1 | sed -e "s/^/DATE /" -e 's/  */,/g' > $HOME/test_runs/$RUN_DATE/${EXECUTABLE_FILENAME}_top.csv
 
-TTY_NAMES=`ps aux | grep ${EXECUTABLE_FILENAME} | grep build/debug | grep -v '\.sh' | grep -v grep | awk '{print $7}'| sort | sed -e 's/^/RSS_/' | awk '{printf(",%s",$NF)}'`
+TTY_NAMES=`ps aux | grep ${EXECUTABLE_FILENAME} | grep build/debug | grep -v '\.sh' | grep -v heaptrack | grep -v valgrind | grep -v grep | awk '{print $7}'| sort | sed -e 's/^/RSS_/' | awk '{printf(",%s",$NF)}'`
 
 echo "DATE$TTY_NAMES" > $HOME/test_runs/$RUN_DATE/${EXECUTABLE##*/}_memory.csv
 
 while [ : ] ; do
-	MEM=`ps aux | grep ${EXECUTABLE_FILENAME} | grep build/debug | grep -v '\.sh' | grep -v grep | awk '{print $7,$6}'|sort | awk '{printf(",%s",$NF)}'`
+	MEM=`ps aux | grep ${EXECUTABLE_FILENAME} | grep build/debug | grep -v '\.sh' | grep -v heaptrack | grep -v valgrind | grep -v grep | awk '{print $7,$6}'|sort | awk '{printf(",%s",$NF)}'`
 	DATE=`date '+%s'`
 
 	if [ "$MEM" != "$LAST_MEM" ] ; then
